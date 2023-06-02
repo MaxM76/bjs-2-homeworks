70ef40dba3e69259d84a29e3f4ebd3e848291b76
MaxM76 <m.v.markelov@mail.ru>
2023-06-02 18:24:30 +0300

    Third task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
 function compareArrays(arr1, arr2) {
-  
+	let result = false;
+	if (arr1.length === arr2.length) {
+		for (let i = 0; i < arr1.length; i++) {
+			if (arr1[i] !== arr2[i]) {
+				return result;
+			}
+		}
+		result = true;
+	}
+	return result;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	return users
+		.filter((user) => user.gender === gender)
+		.map((user) => user.age)
+		.reduce((avr, age, index, ages) => (ages.length === 0) ? 0 : avr + age / ages.length, 0);
 }
